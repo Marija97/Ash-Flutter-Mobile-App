bea8607f557bda6983c58b9cf08d061dfb5dc314
Marija <marija.vujica97@gmail.com>
2024-02-05 19:44:45 +0100

    Image of a photo album for decoration

--- lib/pages/home/feed/feed_page.dart
+++ lib/pages/home/feed/feed_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../../../common/colors.dart';
+import '../../../gen/assets.gen.dart';
 import '../../../widgets/text.dart';
 
 class FeedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AshText.large('Feed :)'),
+    return Container(
+      color: AshColors.photoAlbumBackground,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AshText.large('Feed :)'),
+          Assets.images.illustrations.photoAlbum.image(),
+        ],
+      ),
     );
   }
 }
