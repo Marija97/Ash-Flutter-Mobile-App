7c3348b37e74092ce539ab9ea49fb3c21ced141b
Marija Vujica <marija.vujica97@gmail.com>
2024-08-13 15:38:34 +0200

    Project restructuring

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'package:ash/theme/theme.dart';
-import 'routing/router.dart';
+import 'app/routing/router.dart';
+import 'app/theme/theme.dart';
 
 Future<void> main() async {
   await Future.delayed(const Duration(seconds: 1));
